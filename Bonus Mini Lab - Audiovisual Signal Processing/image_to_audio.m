051909a87c37cb1e5f291a736b7f52e036b08ce3
nosaesa <jtlievense@gmail.com>
2014-03-03 15:14:38 -0800

    added mini bonus lab

---
+++ Bonus Mini Lab - Audiovisual Signal Processing/image_to_audio.m
@@ -0,0 +1,9 @@
+% Use sound(x,fs) to listen to a 1D array x (either column or row, it
+% doesn't matter). Don't change the variable fs until you've deciphered the
+% message. Once you're finished, try playing around with the sampling
+% rate (e.g. sound(x,fs/2), sound(x,2*fs), etc...). What's going on?!?
+% This will make sense when you cover sampling of continuous time signals.
+
+load('audio_image.mat')
+imshow(av_matrix)
+fs = 8192;
